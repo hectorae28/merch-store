6be6793bd789c45e9517d9b4594855a3f13469ba
Hector Archer <hectorae28@gmail.com>
2022-08-21 21:37:56 -0400

    add credentials.js

---
+++ credentials.js
@@ -0,0 +1,6 @@
+const credentials = {
+  paypal_clientId:
+    'AVfgyzve1EBAP6IjlqHFjB7q4xOA7Fd4aCvfYWGoJ98Tx6MdtJcF9beaWU85w6UnhtpHz1FM0Cf8yfEu',
+  positionstack_access_key: '3d42b93b76ede1683d2b67cebfd2ed0b',
+};
+export default credentials;
