7fcb40016ac5cf8a412108a53c7d19d80f5ed7a3
Hector Archer <hectorae28@gmail.com>
2023-01-18 20:59:20 -0400

    cambio env

--- credentials.js
+++ credentials.js
@@ -1,7 +1,7 @@
 //require('dotenv').config()
 const credentials = {
   paypal_clientId:
-    process.env.REACT_APP_PAYPAL_CLIENT_ID,
+    process.env.REACT_APP_PAYPAL_CLIENT_ID||secrets.REACT_APP_PAYPAL_CLIENT_ID,
   positionstack_access_key: process.env.REACT_APP_POSITIONSTACK_ACCESS_KEY,
 };
 export default credentials;
